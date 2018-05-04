6eb00449cd75614a5c123940749b43a57cee6199
nihao <nihao@meituan.com>
2018-05-04 15:32:48 +0800

    practice1 done

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # python-practice
  My personal practices on https://www.practicepython.org/
+
+### Practice1
+- 获取用户输入`input`函数；
+- int()方法将string转为整数；
+- datetime模块；
+- python的命名风格；
--- src/practice1/practice-1.py
+++ src/practice1/practice-1.py
@@ -1,5 +1,27 @@
-def main():
-    print("jjj")
+import datetime
+
+
+def year():
+    name = input("please input your name:")
+    age = int(input("please input your age:"))
+    now = datetime.datetime.now()
+
+    # python中的局部变量是小写字母，用_分隔;
+    year_gap = 100 - age
+    print('hello %s, you will become 100 years old in year %d' % (name, now.year + year_gap))
+
+
+def repeat():
+    msg = input("please some message:")
+    count = int(input('please input the repeat count:'))
+    print(msg * count)
+
+
+def repeat_newline():
+    msg = input("please some message:")
+    count = int(input('please input the repeat count:'))
+    print((msg + '\n') * count)
+
 
 if __name__ == '__main__':
-    main()
+    year()
